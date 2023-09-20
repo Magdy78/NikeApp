adf024047d3414b46334e1551a69afb3396487e9
Magdy78 <azzam9778@gmail.com>
2023-09-21 00:04:37 +0300

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,8 @@ const App = () => (
   
   <main className="relative">
     <Nav />
-    <section className="xl: padding-1 wide:padding-r padding-b"><Hero/></section>
-    <section className="padding"> <PopularProducts /></section>
+    <section className="xl: padding-1 wide:padding-r padding-b" style={{paddingLeft :"9vh"}}><Hero/></section>
+    <section className="padding " > <PopularProducts /></section>
     <section className="padding"> <SuperQuality /></section>
     <section className="padding-x py-10"> <Services /></section>
     <section className="padding"> <SpecialOffers /> </section>
