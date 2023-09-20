ae973b805a56ce5b043c0e69872f7bf72fc6f109
Magdy78 <azzam9778@gmail.com>
2023-09-21 00:17:37 +0300

    Final Code

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ const App = () => (
     <section className="padding"> <SuperQuality /></section>
     <section className="padding-x py-10"> <Services /></section>
     <section className="padding"> <SpecialOffers /> </section>
-    <section className="bg-pale-blue padding"> <CustomerReviews /> </section>
+    {/* <section className="bg-pale-blue padding"> <CustomerReviews /> </section> */}
     <section className="padding-x sm:py-32 py-16 w-full"><Subscribe /></section>
     <section className=" bg-black padding-x padding-t pb-8"><Footer /></section>
   </main>
